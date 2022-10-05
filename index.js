063e93a651f546a49ba75b17bcf385ace70c7067
Tim <timotheecronier@gmail.com>
2022-10-05 09:34:00 +0200

    test

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-// Import a module
-/*
 const http = require('http')
+const handles = require('./handles.js')
+const port = 8080
 
-const serverHandle = function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }
-  
-  const server = http.createServer(serverHandle);
-  server.listen(8080) */
+http.createServer(serverHandle).listen(port, () => console.log(`Server created at localhost:$port`)) 
 
 
-const http = require('http')
 
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello zebi!</p>' +
-'    </body>' +
-'</html>'
 
-/*const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
-  res.end();
-}*/
-
-const serverHandle = function (req, res) {
-    // Retrieve and print the current path
-    const path = url.parse(req.url).pathname;
-    console.log(path);
-  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(path);
-    res.end();
-  }
-
-const server = http.createServer(serverHandle);
-server.listen(8080) 
-
-// Import Node url module
-const url = require('url')
-
+ 
