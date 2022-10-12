9c0d42df9cd21bbf5c7a07c36c005dee2982c340
Tim <timotheecronier@gmail.com>
2022-10-12 09:35:08 +0200

    deleting file

--- handles.js
+++ handles.js
@@ -15,47 +15,7 @@ router.get('/hello/:name', (req, res) => {
 })
 
 
-/*
-module.exports = {
-    serverHandle: function (req, res) {
-        const route = url.parse(req.url)
-        const path = route.pathname 
-        const params = qs.parse(route.query)
-        
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        switch (path) {
-            case '/':
-                res.end('You need to enter "/hello" and a name, so this text would be "hello [name] !')
-                break;
-            case '/hello':
-                if ('name' in params && (["Tim","Sami"].includes(params.name))) res.end('Hello, I\'m Tim and I\'m 20 years old.')
-                else res.end('Hello '+ params.name)
-                break;
-            case '/about':
-                res.end(JSON.stringify(aboutus))
-                break;
-            default:
-                res.end("Error 404 : route not found")    
-        }
-    }   
-}
-
-
-// fs.readFile("./about.json", "utf8", callback(err, jsonString))
-
-function callback(err, jsonString) {
-    if (err) {
-        console.log("File read failed:", err)
-        return
-    }
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    aboutus = JSON.parse(jsonString)
-    console.log(typeof aboutus)
-    res.write(JSON.stringify(aboutus))
-}
-
-*/
-module.exports = router
+module.exports = router;
 
 
 
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 const port = 8080
 
 app.use(express.json())
-// app.use(express.urlencoded())
 
 app.use('/', router)
 
