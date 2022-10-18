80b59b7444444b200607722b105a4aca08a042ae
Tim <timotheecronier@gmail.com>
2022-10-19 01:55:24 +0200

    app w some pages

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+// pages/about.js
+
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h2>
+                About us 
+            </h2>
+            <p>
+                This project is being made by Timothée Cronier and Sami
+            </p>
+        </div>
+    )
+}
---
+++ pages/contacts.js
@@ -0,0 +1,19 @@
+// pages/about.js
+
+export default function Contacts() {
+    return(
+        <div>
+            <h2>
+                Contacts
+            </h2> <p></p>
+            <li>
+                Timothée Cronier - timothee.cronier@edu.ece.fr
+            </li> 
+            <li>
+                Sami - jesaispas@gmail.com
+            </li>
+        </div>
+        
+        
+    )
+}
