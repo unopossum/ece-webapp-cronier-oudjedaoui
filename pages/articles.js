ba27e9e9871abaf32fc223fc8eefe2028479e92a
Tim <timotheecronier@gmail.com>
2022-10-19 02:57:58 +0200

    fixing some issues

--- pages/articles.js
+++ pages/articles.js
@@ -1,5 +1,7 @@
 // pages/articles.js
 
+import { useRouter } from 'next/router'
+
 export default function Articles() {
     return (
         <div>
@@ -7,35 +9,21 @@ export default function Articles() {
                 <li>
                     Article 1
                     <ul>
-                        <li>
-                            Name : Nike AIRMAX Lemonade
-                        </li>
-                        <li>
-                            Size : 45
-                        </li>
-                        <li>
-                            Color : green
-                        </li>
-                        <li>
-                            Price : 225€
-                        </li>
+                        <li>Name : Nike AIRMAX Lemonade</li>
+                        <li>Size : 45</li>
+                        <li>Color : green</li>
+                        <li>Price : 225€</li>
+                        <li>ID : 123456789</li>
                     </ul>
                 </li>
                 <li>
                     Article 2
                     <ul>
-                        <li>
-                            Name : Nike AirForce 07
-                        </li>
-                        <li>
-                            Size : 45
-                        </li>
-                        <li>
-                            Color : white
-                        </li>
-                        <li>
-                            Price : 100€
-                        </li>
+                        <li>Name : Nike AirForce 07</li>
+                        <li>Size : 45</li>
+                        <li>Color : white</li>
+                        <li>Price : 100€</li>
+                        <li>ID : 987654321</li>
                     </ul>
                 </li>
             </ul>
